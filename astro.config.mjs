0f19123e68e1daf556dc3e4a1622c075a03b2832
FraserMuir <frasermuir14@gmail.com>
2022-12-30 19:13:57 +0000

    re-add compression

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,9 @@
 import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
+import compress from "astro-compress";
 
 export default defineConfig({
   site: "https://blog-beta-six-37.vercel.app",
-  integrations: [
-    sitemap(),
-    tailwind(),
-  ],
+  integrations: [sitemap(), tailwind(), compress()],
 });
