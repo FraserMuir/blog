a38c6b3cde444412930d9b49716850add92ca000
FraserMuir <frasermuir14@gmail.com>
2023-01-05 19:06:25 +0000

    remove tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,8 @@
 import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
-import tailwind from "@astrojs/tailwind";
 import compress from "astro-compress";
 
 export default defineConfig({
   site: "https://blog-beta-six-37.vercel.app",
-  integrations: [
-    sitemap(),
-    tailwind({
-      config: { applyBaseStyles: false },
-    }),
-    compress(),
-  ],
+  integrations: [sitemap(), compress()],
 });
