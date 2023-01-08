fc6090377ae6fd19de8099eae8347f6d4c4574e8
FraserMuir <frasermuir14@gmail.com>
2023-01-08 23:27:10 +0000

    add config to watch markdown files

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,23 @@ import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
 import compress from "astro-compress";
 
+const watch = () => {
+  return {
+    name: "watch-markdown-files",
+    handleHotUpdate({ file, server }) {
+      if (file.endsWith(".md")) {
+        server.ws.send({
+          type: "full-reload",
+        });
+      }
+    },
+  };
+};
+
 export default defineConfig({
   site: "https://blog-beta-six-37.vercel.app",
   integrations: [sitemap(), compress()],
+  vite: {
+    plugins: [watch()],
+  },
 });
